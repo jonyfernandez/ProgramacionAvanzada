17cd3fee850c81ef7c2979f8ee1e03878f65b71a
Jonathan Fernández <jony.a.fernandez@gmail.com>
2019-06-03 17:24:25 -0300

    Se actualizacion ejercicios de prolog

--- Prolog/Arbol.pl
+++ Prolog/Arbol.pl
@@ -13,9 +13,26 @@ padre(pedro, maría) .
 padre(jony,marta).
 padre(jony,homero).
 
+%NIÑO(X,Y): EXPRESA QUE X ES HIJO O HIJA DE Y.
+
 niño(X,Y):-padre(Y,X).
+
+%HIJO(X,Y): EXPRESA QUE X ES UN HIJO VARON DE Y.
+
 hijo(X,Y):-padre(Y,X),hombre(X).
+
+%HIJA(X,Y): EXPRESA QUE X ES UNA HIJA DE Y.
+
 hija(X,Y):-padre(Y,X),mujer(X).
+
+%HERMANO_O_HERMANA(X,Y): EXPRESA QUE X ES HERMANO O HERMANA DE Y.
+
 hermano_o_hermana(X,Y):-padre(Z,X),padre(Z,Y),not(X==Y).
+
+%HERMANO(X,Y): EXPRESA QUE X ES HERMANO DE Y.
+
 hermano(X,Y):-hermano_o_hermana(X,Y),hombre(X).
+
+%HERMANA(X,Y): EXPRESA QUE X ES HERMANA DE Y.
+
 hermana(X,Y):-hermano_o_hermana(X,Y),mujer(X).
